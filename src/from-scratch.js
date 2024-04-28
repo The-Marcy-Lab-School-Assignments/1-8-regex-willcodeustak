c624930a29c7c8c9aefa3d137c92eb1013e5b5de
WilliamZeng <itswilliamzeng@gmail.com>
2024-04-28 00:40:27 -0400

    update

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,14 +1,32 @@
-const helloWorldRegex = (str) => {};
+const helloWorldRegex = (str) => {
+  const onlyHelloWorld = /hello\s+world/gi;
+  return onlyHelloWorld.test(str);
+};
+const hasAVowel = (str) => {
+  const trueIfAtLeast1Vowel = /[aeiou]/gi;
 
-const hasAVowel = (str) => {};
+  return trueIfAtLeast1Vowel.test(str);
+};
 
-const hasCatsOrDogs = (str) => {};
+const hasCatsOrDogs = (str) => {
+  const trueIfCatsOrDogs = /cats|dogs/gi;
+  return trueIfCatsOrDogs.test(str);
+};
 
-const hasVowelStart = (str) => {};
+const hasVowelStart = (str) => {
+  const trueIfStartsWithVowel = /^[aeiou]/gi;
+  return trueIfStartsWithVowel.test(str);
+};
 
-const hasPunctuationEnd = (str) => {};
+const hasPunctuationEnd = (str) => {
+  const trueIfEndWithPeriod = /[.!?]$/g;
+  return trueIfEndWithPeriod.test(str);
+};
 
-const hasNothingOrDigits = (str) => {};
+const hasNothingOrDigits = (str) => {
+  const emptyOrDigits = /^(|\d+)$/g;
+  return emptyOrDigits.test(str);
+};
 
 const hasNoFlippers = (str) => {};
 
